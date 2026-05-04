388b59b6a11f7f4af9e6c65da3c82596483498b0
MonoxTI <itumonokoane84@gmail.com>
2026-05-04 10:23:24 +0200

    size

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ export default function HomePage() {
               <img 
                 src="/3.png" 
                 alt="novaa" 
-                className="h-32 md:h-60 lg:h-81 w-auto object-contain"
+                className="h-32 md:h-60 lg:h-150 w-auto object-contain"
                 style={{ mixBlendMode: "multiply" }} 
               />
             </div>
