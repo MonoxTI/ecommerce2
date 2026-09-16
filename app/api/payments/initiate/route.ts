59dc822ce148aaac2832f4376bb70ce4a56665af
MonoxTI <itumonokoane84@gmail.com>
2026-09-16 14:31:09 +0200

    fix2

--- app/api/payments/initiate/route.ts
+++
@@ -1,7 +0,0 @@
-// app/api/payments/initiate/route.ts
-import { NextRequest } from "next/server";
-import { handleInitiatePaystackPayment } from "@/lib/payments/paystack-handlers";
-
-export async function POST(req: NextRequest) {
-  return handleInitiatePaystackPayment(req);
-}
