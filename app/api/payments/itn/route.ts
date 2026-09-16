01c4d7fc49fca96459f3d50dd75c2b5b5fd9d541
MonoxTI <itumonokoane84@gmail.com>
2026-09-16 14:37:38 +0200

    fix4

--- app/api/payments/itn/route.ts
+++
@@ -1,9 +0,0 @@
-// app/api/payments/itn/route.ts
-import { NextRequest } from "next/server";
-import { handlePayFastITN } from "@/lib/payments/handlers";
-
-export const dynamic = "force-dynamic";
-
-export async function POST(req: NextRequest) {
-  return handlePayFastITN(req);
-}
